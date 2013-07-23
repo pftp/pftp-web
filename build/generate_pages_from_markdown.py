b48e8a4c86d11a894abcc88e6e093e15c1853483
Hurshal Patel <hpatel516@gmail.com>
2013-07-23 00:39:44 -0700

    humanize markdown template titles and import script

--- build/generate_pages_from_markdown.py
+++ build/generate_pages_from_markdown.py
@@ -2,28 +2,37 @@ import os
 import markdown
 from markdown.postprocessors import Postprocessor
 
-markdown_template_file = open('templates/markdown_template.html', 'r')
-markdown_template = markdown_template_file.read()
+def humanize(filename):
+  uncapitalized = ['a', 'an', 'and', 'at', 'by', 'from', 'of', 'on', 'or', 'the', 'to', 'with', 'without']
+  name = os.path.splitext(filename)[0]
+  words = [word[0].upper() + word[1:] if word not in uncapitalized else word for word in name.split('_')]
+  title = ' '.join(words)
+  title = title[0].upper() + title[1:]
+  return title
 
-processor = markdown.Markdown()
+if __name__ == '__main__':
+  markdown_template_file = open('templates/markdown_template.html', 'r')
+  markdown_template = markdown_template_file.read()
+
+  processor = markdown.Markdown()
 
 
-for root, subFolders, files in os.walk('markdown'):
-  for filename in files:
-    input_path = os.path.join(root, filename)
-    output_dir = root.replace('markdown', 'templates/gen', 1)
-    output_path = os.path.splitext(os.path.join(output_dir, filename))[0] + '.html'
-    input_file = open(input_path, 'r')
-    input_text = input_file.read()
+  for root, subFolders, files in os.walk('markdown'):
+    for filename in files:
+      input_path = os.path.join(root, filename)
+      output_dir = root.replace('markdown', 'templates/gen', 1)
+      output_path = os.path.splitext(os.path.join(output_dir, filename))[0] + '.html'
+      input_file = open(input_path, 'r')
+      input_text = input_file.read()
 
-    if not os.path.exists(output_dir):
-      os.makedirs(output_dir)
-    output_file = open(output_path, 'w')
+      if not os.path.exists(output_dir):
+        os.makedirs(output_dir)
+      output_file = open(output_path, 'w')
 
-    print "Generating %s from %s" % (output_path, input_path)
-    html_output = processor.convert(input_text)
+      print "Generating %s from %s" % (output_path, input_path)
+      html_output = processor.convert(input_text)
 
-    output_text = markdown_template.replace('[title]', filename).replace('[content]', html_output)
-    output_file.write(output_text)
+      output_text = markdown_template.replace('[title]', humanize(filename)).replace('[content]', html_output)
+      output_file.write(output_text)
 
-    processor.reset()
+      processor.reset()
