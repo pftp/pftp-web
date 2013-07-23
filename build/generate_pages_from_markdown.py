6956d6260db4c5cb972ec69d4bd5c3933caa54e2
Hurshal Patel <hpatel516@gmail.com>
2013-07-23 00:29:42 -0700

    add markdown template and insert markdown text in build script

--- build/generate_pages_from_markdown.py
+++ build/generate_pages_from_markdown.py
@@ -1,7 +1,12 @@
 import os
 import markdown
+from markdown.postprocessors import Postprocessor
+
+markdown_template_file = open('templates/markdown_template.html', 'r')
+markdown_template = markdown_template_file.read()
 
 processor = markdown.Markdown()
+
 
 for root, subFolders, files in os.walk('markdown'):
   for filename in files:
@@ -9,12 +14,16 @@ for root, subFolders, files in os.walk('markdown'):
     output_dir = root.replace('markdown', 'templates/gen', 1)
     output_path = os.path.splitext(os.path.join(output_dir, filename))[0] + '.html'
     input_file = open(input_path, 'r')
+    input_text = input_file.read()
 
     if not os.path.exists(output_dir):
       os.makedirs(output_dir)
     output_file = open(output_path, 'w')
 
     print "Generating %s from %s" % (output_path, input_path)
-    processor.convertFile(input=input_file, output=output_file)
+    html_output = processor.convert(input_text)
+
+    output_text = markdown_template.replace('[title]', filename).replace('[content]', html_output)
+    output_file.write(output_text)
 
     processor.reset()
