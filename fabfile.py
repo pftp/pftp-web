048bfebe12f6f01d29bda9925bfe5fdaa02b85f0
Hurshal Patel <hpatel516@gmail.com>
2013-07-23 01:51:00 -0700

    add sqlite3 init to fabfile

--- fabfile.py
+++ fabfile.py
@@ -4,4 +4,6 @@ def build():
   print 'Building site resources'
   local('rm -rf templates/gen')
   local('python build/generate_pages_from_markdown.py')
+  local('rm pftp.db')
+  local('sqlite3 pftp.db < schema.sql')
   local('python build/add_exercises_to_db.py')
