2a9aae92f8dc0c257e5ae9db53fbdc321e0c4c24
Ben Sklaroff <bsklaroff@gmail.com>
2013-07-26 01:48:03 -0700

    add output to practice page

--- static/practice/practice.js
+++ static/practice/practice.js
@@ -1,3 +1,13 @@
+function outf(text) {
+  var mypre = document.getElementById("output");
+  mypre.innerHTML = mypre.innerHTML + text;
+}
+function builtinRead(x) {
+  if (Sk.builtinFiles === undefined ||
+      Sk.builtinFiles["files"][x] === undefined)
+    throw "File not found: '" + x + "'";
+  return Sk.builtinFiles["files"][x];
+}
 $(function() {
   var editor;
   editor = CodeMirror.fromTextArea(document.getElementById('code'), {
@@ -6,5 +16,13 @@ $(function() {
     lineNumbers: true,
     indentUnit: 4,
     mode: 'python'
+  });
+  $('#run_code').click(function(e) {
+    var code;
+    code = editor.getValue().replace(/\t/g, '    ');
+    Sk.canvas = "turtle_canvas";
+    Sk.pre = "output";
+    Sk.configure({output: outf, read: builtinRead});
+    eval(Sk.importMainWithBody("<stdin>", false, code));
   });
 });
