8552b2e977700cb56a434fed42351986ff858a67
Hurshal Patel <hpatel516@gmail.com>
2013-07-27 12:55:46 -0700

    clean up add_exercises

--- build/add_exercises_to_db.py
+++ build/add_exercises_to_db.py
@@ -1,6 +1,11 @@
 import json
 import sqlite3
+from termcolor import colored
 
+"""
+Adds all the exercies from the json blob into the database.
+Run from fabfile with fab build
+"""
 if __name__ == '__main__':
     exercises_file = open('static/practice/exercises.json', 'r')
     exercises = json.loads(exercises_file.read())
@@ -12,4 +17,4 @@ if __name__ == '__main__':
                        ex['solution']])
     db.commit()
     db.close()
-    print exercises
+    print colored("%s exercises addded to database." % len(exercises), "green")
