1ad98a0f1751b2e422992d9f1bb3c6600e99767a
Hurshal Patel <hpatel516@gmail.com>
2013-07-27 12:46:34 -0700

    clean up application.py

--- application.py
+++ application.py
@@ -3,11 +3,32 @@ import sqlite3
 from contextlib import closing
 from flask import Flask, render_template, redirect, g, Markup
 
+
+####################################################################################
+# Database
+####################################################################################
 DATABASE = 'pftp.db'
 
+def connect_db():
+    return sqlite3.connect(app.config['DATABASE'])
+
+def init_db():
+    with closing(connect_db()) as db:
+        with app.open_resource('schema.sql', mode='r') as f:
+            db.cursor().executescript(f.read())
+        db.commit()
+
+
+####################################################################################
+# App
+####################################################################################
 app = Flask(__name__)
 app.config.from_object(__name__)
 
+
+####################################################################################
+# Routes
+####################################################################################
 @app.route('/')
 def index():
   return 'This is Programming Feel The Power'
@@ -32,15 +53,6 @@ def lesson(path):
   else:
     return redirect('/')
 
-def connect_db():
-    return sqlite3.connect(app.config['DATABASE'])
-
-def init_db():
-    with closing(connect_db()) as db:
-        with app.open_resource('schema.sql', mode='r') as f:
-            db.cursor().executescript(f.read())
-        db.commit()
-
 @app.before_request
 def before_request():
     g.db = connect_db()
@@ -52,5 +64,9 @@ def teardown_request(exception):
     if db is not None:
         db.close()
 
+
+####################################################################################
+# Runner
+####################################################################################
 if __name__ == '__main__':
   app.run(debug=True)
