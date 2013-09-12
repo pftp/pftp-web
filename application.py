943e82732cb1abe0870b779f817e09c33c5ced00
Hurshal Patel <hpatel516@gmail.com>
2013-09-12 01:55:46 -0700

    last name doesnt have to be unique

--- application.py
+++ application.py
@@ -46,7 +46,7 @@ class User(db.Model, UserMixin):
   id = db.Column(db.Integer(), primary_key=True)
   email = db.Column(db.String(120), index = True, unique = True, nullable = False)
   firstname = db.Column(db.String(30), index = True, unique = True, nullable = False)
-  lastname = db.Column(db.String(30), index = True, unique = True, nullable = False)
+  lastname = db.Column(db.String(30), index = True, nullable = False)
   password = db.Column(db.String(255), nullable = False)
   active = db.Column(db.Boolean())
   confirmed_at = db.Column(db.DateTime())
