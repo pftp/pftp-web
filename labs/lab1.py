1f4b46ac8ac5bd5c7c18f7ef5f9ed8b0679a0966
Hurshal Patel <hpatel516@gmail.com>
2013-09-15 23:58:27 -0700

    update lab1 content to match old lab.js

--- labs/lab1.py
+++ labs/lab1.py
@@ -1,52 +1,21 @@
-import turtle
-import time
+#Welcome to Lab 1! We are going to learn some turtle graphics today. Hit the 'Next' button to get started.
 
-#Wrapper class to display messages and pause between steps
-class Turtle(object):
-  def __init__(self):
-    turtle.bgcolor("black")
-    self.turtle = turtle.Turtle()
-    self.turtle.pencolor("white")
+# This is Winston, your guide to the magic that is Python.
+# Don't worry about what `import` or `turtle.Turtle()` means - we will get into that later. All that matters is that we have a turtle and his name is Winston. He will listen to your instructions and execute them in order with no mistakes because he is just that good.
+###import turtle
+###winston = turtle.Turtle()
 
-  def forward(self, amt):
-    self.turtle.forward(amt)
-    time.sleep(3)
+# You can call Winston by his name and tell him to move! Try adding a line at the end that says `winston.forward(100)`. This tells Winston to move 100 units in the direction he's facing. Hit the 'Run It!' button when you're done to see Winston in action.
 
-  def backward(self, amt):
-    self.turtle.backward(amt)
-    time.sleep(3)
+# Now try bringing Winston back to where he started with `winston.backward()` (You still need to supply a number between the parentheses so Winston knows how far to go.
 
-  def left(self, amt):
-    self.turtle.left(amt)
-    time.sleep(3)
+# Winston can even change directions! Try telling Winston to turn right by adding a line at the end that says `winston.right(90)`.
 
-  def right(self, amt):
-    self.turtle.right(amt)
-    time.sleep(3)
+# Try making Winston spin in a circle counter-clockwise. (Hint: Winston will have to turn left and he only understands angles in degrees.
 
-  def say(self, msg):
-    self.turtle.write(msg)
-    time.sleep(3)
-
-
-# This is Winston, your chelonian (that means turtle) guide to the magic that is python.
-winston = turtle.Turtle()
-
-# you can tell Winston to move!
-winston.forward(100)
-winston.backward(100)
-
-## Give it a try! Make Winston cross the street
-# winston.forward(100)
-
-# you can also tell Winston to turn!
-winston.left(90)
+# Now Winston is pretty smart for a turtle, and using just these commands, you can tell him to draw shapes. Try adding these lines at the end:
 winston.right(90)
-
-## Tell Winston to spin in a circle clockwise
-# winston.right(360)
-
-# by combining these instructions, you can tell winston to make shapes
+winston.forward(100)
 winston.right(90)
 winston.forward(100)
 winston.right(90)
@@ -54,16 +23,11 @@ winston.forward(100)
 winston.right(90)
 winston.forward(100)
 
-## Now make Winston draw a triangle
+# Now it's your turn! Make Winston draw a triangle. You can edit or delete the lines from the previous step to do this.
 
-# Winston normally drags a pen behind him when he moves, but you can tell him to carry it.
-# Hey Winston, stop slacking off and carry the pen
-winston.penup()
-# now when winston moves, he won't draw anything
-winston.forward(100)
+# Cool! Now normally Winston drags a pen behind him when he moves, and this leaves the lines that you see. You can tell Winston to stop slacking off and carry the pen if you don't want him to draw: `winston.penup()`. Try adding this line and then make Winston move. You should see that there is no line behind him as he moves this time!
 
-# you can use this to tell winston to make separate shapes
-# Ok winston you can draw again
+# You can use this to make separate shapes! Just tell Winston to put his pen back down and draw a shape, and then pick it up while he moves to a different spot to start the next shape. Try adding these lines to see it:
 winston.pendown()
 winston.right(90)
 winston.forward(100)
@@ -71,11 +35,11 @@ winston.right(90)
 winston.forward(100)
 winston.right(90)
 winston.forward(100)
-
+winston.right(90)
+winston.forward(100)
 winston.penup()
-winston.forward(100)
+winston.forward(200)
 winston.pendown()
-
 winston.right(120)
 winston.forward(100)
 winston.right(120)
@@ -83,8 +47,155 @@ winston.forward(100)
 winston.right(120)
 winston.forward(100)
 
-## now try making a pentagon and a hexagon with some space in between
+# Now you try making a pentagon inside of a hexagon.
 
-# cool! your assignment for this week is to make a picture of something
+# Winston also has one of those giant packs of crayons, so you can tell him to change the color he draws too: `winston.color("green")`. He will understand the following list of colors:
+aliceblue
+antiquewhite
+aqua
+aquamarine
+azure
+beige
+bisque
+black
+blanchedalmond
+blue
+blueviolet
+brown
+burlywood
+cadetblue
+chartreuse
+chocolate
+coral
+cornflowerblue
+cornsilk
+crimson
+cyan
+darkblue
+darkcyan
+darkgoldenrod
+darkgray
+darkgreen
+darkgrey
+darkkhaki
+darkmagenta
+darkolivegreen
+darkorange
+darkorchid
+darkred
+darksalmon
+darkseagreen
+darkslateblue
+darkslategray
+darkslategrey
+darkturquoise
+darkviolet
+deeppink
+deepskyblue
+dimgray
+dimgrey
+dodgerblue
+firebrick
+floralwhite
+forestgreen
+fuchsia
+gainsboro
+ghostwhite
+gold
+goldenrod
+gray
+green
+greenyellow
+grey
+honeydew
+hotpink
+indianred
+indigo
+ivory
+khaki
+lavender
+lavenderblush
+lawngreen
+lemonchiffon
+lightblue
+lightcoral
+lightcyan
+lightgoldenrodyellow
+lightgray
+lightgreen
+lightgrey
+lightpink
+lightsalmon
+lightseagreen
+lightskyblue
+lightslategray
+lightslategrey
+lightsteelblue
+lightyellow
+lime
+limegreen
+linen
+magenta
+maroon
+mediumaquamarine
+mediumblue
+mediumorchid
+mediumpurple
+mediumseagreen
+mediumslateblue
+mediumspringgreen
+mediumturquoise
+mediumvioletred
+midnightblue
+mintcream
+mistyrose
+moccasin
+navajowhite
+navy
+oldlace
+olive
+olivedrab
+orange
+orangered
+orchid
+palegoldenrod
+palegreen
+paleturquoise
+palevioletred
+papayawhip
+peachpuff
+peru
+pink
+plum
+powderblue
+purple
+red
+rosybrown
+royalblue
+saddlebrown
+salmon
+sandybrown
+seagreen
+seashell
+sienna
+silver
+skyblue
+slateblue
+slategray
+slategrey
+snow
+springgreen
+steelblue
+tan
+teal
+thistle
+tomato
+turquoise
+violet
+wheat
+white
+whitesmoke
+yellow
+yellowgreen
 
-turtle.getscreen()._root.mainloop()
+# Awesome! Now your assignment for this week is to make a picture of something cool
