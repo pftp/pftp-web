b8e9f83120350b6003dbf07759349984fa627bdf
Hurshal Patel <hpatel516@gmail.com>
2013-09-28 00:37:09 -0700

    fix grade submission for admin

--- static/js/admin.js
+++ static/js/admin.js
@@ -1,29 +1,37 @@
-var students = new Array();
 $(document).ready(function() {
   $('#student_name').autocomplete({
-    source: students
+      source: students
   });
-  $('#assignment_name').change(function(e) {
-    $('#max_score').html('out of ' + assignment_to_score[$('#assignment_name').val()]);
-  });
-  $('#assignment_score').keypress(function(e) {
-    if (e.which == 13) {
-      data = {};
-      data['assignment_name'] = $('#assignment_name').val();
-      data['student_name'] = $('#student_name').val();
-      data['score'] = $('#assignment_score').val();
-      $.ajax({
-        type: 'POST',
-        url: '/admin/submit_grade/',
-        data: JSON.stringify(data),
-        contentType: 'application/json; charset=utf-8',
-        dataType: 'json',
-        success: function(g) {
-          $('#student_name').val('');
-          $('#score').val('');
-          $('#student_name').focus();
-        }
-      });
+
+  $('#submit_grade').click(function() {
+    var assignment = $('#assignment_id').val();
+    var name = $('#student_name').val();
+    var score = $('#assignment_score').val();
+
+    if (!assignment || !name || !score)
+      return;
+
+    data = new FormData();
+    data.append('assignment_id', assignment);
+    data.append('score', score);
+
+    for (var id in id_map) {
+      if (id_map[id] === name) {
+        data.append('user_id', id);
+
+        $.ajax({
+            type: 'POST',
+            url: '/admin/submit_grade/',
+            data: data,
+            contentType: false,
+            processData: false,
+            success: function(g) {
+              $('#student_name').val('');
+              $('#score').val('');
+              $('#student_name').focus();
+            }
+        });
+      }
     }
   });
 });
