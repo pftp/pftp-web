6216f9f72edb5c5cf39904965e77fef450ae527a
Hurshal Patel <hpatel516@gmail.com>
2013-09-27 23:30:44 -0700

    add lab4 template

---
+++ labs/lab4.py
@@ -0,0 +1,17 @@
+# Recursion lab
+# Type in
+def a(n):
+  return a(n-1)
+###import turtle
+###winston = turtle.Turtle()
+###def a(n):
+###  if n == 0:
+###    winston.forward(10)
+###    winston.backward(10)
+###    winston.right(15)
+###  else:
+###    a(n-1)
+###    winston.forward(10)
+###    a(n-1)
+###
+###a(5)
