0cfed8be402bb141a71bed8b33e9ffab61fc0138
Lu Cheng <lu.cheng@berkeley.edu>
2013-10-27 23:20:58 -0700

    automatically generate expected based on tests

--- practice/gen_questions.py
+++ practice/gen_questions.py
@@ -1,22 +1,24 @@
 import os
 import json
-def read_problem(parent_dir, problem_dir):
-    problem_path = parent_dir + '/' + problem_dir
+import sys
+PARENT_PROBLEM_DIR = 'problems'
+def read_problem(problem_dir):
+    problem_path = PARENT_PROBLEM_DIR + '/' + problem_dir
     prompt = open(problem_path + '/prompt', 'r').read()
-    expected = open(problem_path + '/expected', 'r').read()
+    #expected = open(problem_path + '/expected', 'r').read()
     solution = open(problem_path + '/solution.py', 'r').read()
     test= open(problem_path + '/test.py', 'r').read()
     hint = open(problem_path + '/hint', 'r').read()
     template_vars = json.load(open(problem_path + '/template_vars.json', 'r'))
-    return {'prompt': prompt, 'expected': expected, 'solution': solution, 'test': test, 'hint': hint, 'template_vars': template_vars, 'problem_dir': problem_dir}
+    return {'prompt': prompt, 'solution': solution, 'test': test, 'hint': hint, 'template_vars': template_vars, 'problem_dir': problem_dir}
 
 
 
 """ load the problems """
-problem_dirs = os.listdir('problems')
+problem_dirs = os.listdir(PARENT_PROBLEM_DIR)
 all_problem_info = []
 for problem_dir in problem_dirs:
-    all_problem_info.append(read_problem('problems', problem_dir))
+    all_problem_info.append(read_problem(problem_dir))
 
 
 # created all problems with their templates
@@ -50,9 +52,17 @@ for problem_info in all_problem_info:
             for variable, values in problem_info['template_vars'].items():
                 template_vars[variable] = values[i]
                 prompt = eval_template(replace_template_vars(problem_info['prompt'], template_vars))
-                expected = eval_template(replace_template_vars(problem_info['expected'], template_vars))
+                #expected = eval_template(replace_template_vars(problem_info['expected'], template_vars))
                 solution = eval_template(replace_template_vars(problem_info['solution'], template_vars))
                 test = replace_template_vars(problem_info['test'], template_vars)
+                temp = sys.stdout
+                sys.stdout = open(PARENT_PROBLEM_DIR + '/' + problem_info['problem_dir'] + '/expected', 'w')
+                #create expected
+                exec(solution + '\n' + test)
+                sys.stdout = temp
+                expected = open(PARENT_PROBLEM_DIR +'/' + problem_info['problem_dir'] + '/expected', 'r').read()
+
+
                 hint = problem_info['hint']
                 templated_problems.append({'prompt': prompt, 'expected': expected, 'solution': solution, 'test': test, 'hint': hint, 'problem_dir': problem_info['problem_dir']})
     all_problems.append(templated_problems)
