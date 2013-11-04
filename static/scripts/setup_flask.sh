049bf24d968d32859a06a92dcc7954b3f4d5bf52
root <root@programmingfeelthepower.com>
2013-11-04 19:12:05 +0000

    update flask install script

--- static/scripts/setup_flask.sh
+++ static/scripts/setup_flask.sh
@@ -3,4 +3,8 @@ export PYTHONPATH=$PYTHONPATH:~/python_packages
 echo 'export PYTHONPATH=$PYTHONPATH:~/python_packages' >> ~/.bash_profile
 mkdir -p ~/python_packages
 easy_install --install-dir ~/python_packages flask
-echo -e "\[\033[33m\]Flask installed! Please close this terminal and open a new one to get started.\[\033[0m\]"
+echo ""
+echo ""
+echo "#################################################################################"
+echo "# Flask installed! Please close this terminal and open a new one to get started #"
+echo "#################################################################################"
