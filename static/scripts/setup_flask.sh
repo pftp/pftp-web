78eb539a6900eac091c8895e802e487df405912e
Hurshal Patel <hpatel516@gmail.com>
2013-11-04 11:40:08 -0800

    update flask script

--- static/scripts/setup_flask.sh
+++ static/scripts/setup_flask.sh
@@ -1,4 +1,13 @@
 #!/bin/sh
+if ! type easy_install > /dev/null; then
+    echo ""
+    echo ""
+    echo "############################################################"
+    echo "# You'll need to enter your password to install Flask here #"
+    echo "############################################################"
+    sudo curl https://bitbucket.org/pypa/setuptools/raw/bootstrap/ez_setup.py | python
+fi
+
 export PYTHONPATH=$PYTHONPATH:~/python_packages
 echo 'export PYTHONPATH=$PYTHONPATH:~/python_packages' >> ~/.bash_profile
 mkdir -p ~/python_packages
