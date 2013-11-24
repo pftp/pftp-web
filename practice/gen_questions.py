be9cc088169bfa4edf813dbfeaafe05875a0f78e
Ben Sklaroff <bsklaroff@gmail.com>
2013-11-24 09:59:45 -0800

    move problems from folder to problem directory, and rename them the previous directory names

--- practice/gen_questions.py
+++ practice/gen_questions.py
@@ -5,11 +5,11 @@ import json
 import sys
 PARENT_PROBLEM_DIR = 'problems'
 def read_problem(problem_dir):
-  problem_path = PARENT_PROBLEM_DIR + '/' + problem_dir
-  problem_file = open(problem_path + '/full_problem', 'r')
+  problem_path = PARENT_PROBLEM_DIR + '/' + problem_name
+  problem_file = open(problem_path, 'r')
   current_key = ''
   current_val = ''
-  res = {'problem_dir': problem_dir.strip()}
+  res = {'problem_dir': problem_name.strip()}
   for line in problem_file.readlines():
     if line[:3] == '###':
       if (current_key != ''):
@@ -25,10 +25,10 @@ def read_problem(problem_dir):
 
 
 """ load the problems """
-problem_dirs = os.listdir(PARENT_PROBLEM_DIR)
+problem_names = os.listdir(PARENT_PROBLEM_DIR)
 all_problems = []
-for problem_dir in problem_dirs:
-  all_problems.append(read_problem(problem_dir))
+for problem_name in problem_names:
+  all_problems.append(read_problem(problem_name))
 
 practice_problems = open('practice_problems.json', 'w')
 practice_problems.write(json.dumps(all_problems))
