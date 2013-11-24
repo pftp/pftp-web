f84f6ee1e965866586c7dd05586f7cb6b6b9ce72
Ben Sklaroff <bsklaroff@gmail.com>
2013-11-24 09:42:52 -0800

    allow full problem template to go in one file

--- practice/gen_questions.py
+++ practice/gen_questions.py
@@ -6,6 +6,26 @@ import sys
 PARENT_PROBLEM_DIR = 'problems'
 def read_problem(problem_dir):
     problem_path = PARENT_PROBLEM_DIR + '/' + problem_dir
+
+    # We can put our entire problem in a file called full_problem if we wish
+    if os.path.isfile(problem_path + '/full_problem'):
+      problem_file = open(problem_path + '/full_problem', 'r')
+      current_key = ''
+      current_val = ''
+      res = {'problem_dir': problem_dir.strip()}
+      for line in problem_file.readlines():
+        if line[:3] == '###':
+          if (current_key != ''):
+            res[current_key] = current_val.strip()
+          current_key = line[3:].strip()
+          current_val = ''
+        else:
+          current_val += line
+      if (current_key != ''):
+        res[current_key] = current_val.strip()
+      problem_file.close()
+      return res
+
     prompt = open(problem_path + '/prompt', 'r').read()
     #expected = open(problem_path + '/expected', 'r').read()
     solution = open(problem_path + '/solution.py', 'r').read()
