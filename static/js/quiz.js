2fa4d73061d26989ef461d4af9091707b585e956
Hurshal Patel <hpatel516@gmail.com>
2014-02-28 00:11:38 -0800

    reload quiz page automatically

--- static/js/quiz.js
+++ static/js/quiz.js
@@ -17,7 +17,7 @@ $(function() {
       }
     }).done(function() {
       $('#quiz_container').html('<br><br><br><br><br><h1 style="font-size:1000%"> NICE JOB! </h1>');
-      setTimeout(function() {window.location.href = '/'; }, 1500);
+      setTimeout(function() {window.location.reload(); }, 1500);
     });
   });
 });
